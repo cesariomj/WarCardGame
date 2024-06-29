d5197c48620807567d4816b38d5160f733f8c3f5
mcesario <mcesario@pm.me>
2024-06-29 12:01:52 -0600

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Mark Cesario on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
